c8c0496f8f2da43fd5d1fda3fd9d1d8cf61acf81
lakshyaraj2212 <lakshyarajranawat123@gmail.com>
2024-06-30 20:57:54 +0530

    Create eth+evax.sol

---
+++ eth+evax.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.25;
+
+contract FunctionsAndErrors {
+   
+   function requireNum(uint256 _num) public pure returns(uint256){
+
+    uint256 minValue = 1;
+    require(_num > minValue, "Number must be greater than 1");
+    return _num;
+   }
+
+   function assertNum(uint256 _num) public pure returns(uint256) {
+
+    uint256 valueIncrease = 5;
+    uint256 result = _num + valueIncrease;
+    assert(result > _num);
+    return result;
+   }
+
+   function revertError() public pure {
+    string memory errorMessage = "Transaction reverted.";
+    revert(errorMessage);
+
+   }
+
+}
